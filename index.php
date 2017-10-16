af5d7d60c874b2e1d1b3f70175990bc50970f7f9
waldan <waldancuy@gmail.com>
2017-10-16 07:21:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>GRID</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+<div class="row">
+	<div class="col-lg-12" style="background-color:yellow;">
+      <b>kolom 1</b>
+    </div>
+    <div class="col-lg-12" style="background-color:pink;">
+      <b>kolom 2</b>
+    </div>
+    <div class="col-lg-9" style="background-color:gray;">
+      <b>kolom 3</b>
+    </div>
+    <div class="col-lg-9" style="background-color:#2b00c8;">
+      <b>kolom 4</b>
+    </div>
+	<div class="col-lg-6" style="background-color:gold;">
+      <b>kolom 5</b>
+    </div>
+	<div class="col-lg-6" style="background-color:red;">
+      <b>kolom 6</b>
+    </div>
+	<div class="col-lg-2" style="background-color:orange;">
+      <b>kolom 7</b>
+    </div>
+	<div class="col-lg-2" style="background-color:#99ff01;">
+      <b>kolom 8</b>
+	  </div>
+<div class="container">
+<div class="row">
+	<div class="col-sm-5" style="background-color:#01b5ff;">
+      <b>CONTOH SEBUAH PARAGRAF</b>
+	  <p>Ini adalah contoh sebuah paragraf. Akan menyusut dengan sendirimya ketika berada di tampilan layar smartphone.</p> 
+	</div>
+	<div class="col-sm-5" style="background-color:#da06fc;">
+      <b>CONTOH SEBUAH PARAGRAF 2</b>
+	  <p>Facebook adalah sebuah layanan jejaring sosial berkantor pusat di Menlo Park, California, Amerika Serikat yang diluncurkan pada bulan Februari 2004. Hingga September 2012, Facebook memiliki lebih dari satu miliar pengguna aktif,[8] lebih dari separuhnya menggunakan telepon genggam.[9] Pengguna harus mendaftar sebelum dapat menggunakan situs ini. Setelah itu, pengguna dapat membuat profil pribadi, menambahkan pengguna lain sebagai teman, dan bertukar pesan, termasuk pemberitahuan otomatis ketika mereka memperbarui profilnya. Selain itu, pengguna dapat bergabung dengan grup pengguna dengan ketertarikan yang sama, diurutkan berdasarkan tempat kerja, sekolah atau perguruan tinggi, atau ciri khas lainnya, dan mengelompokkan teman-teman mereka ke dalam daftar seperti "Rekan Kerja" atau "Teman Dekat".</p> 
+	</div>
+</div>
+</div>
+</body>
+</html>
